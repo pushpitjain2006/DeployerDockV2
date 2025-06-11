f24e57fd87fcaa2d699260393daafa45c05e2b3b
Pushpit jain <pj325284@gmail.com>
2025-06-12 01:56:18 +0530

    reverse proxy initial commit

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import httpProxy from "http-proxy";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = 8000;
+
+const proxy = httpProxy.createProxyServer();
+const BucketName = process.env.BucketName;
+const Region = process.env.Region;
+const BasePath = `https://${BucketName}.s3.${Region}.amazonaws.com/__output`;
+
+app.use((req, res) => {
+  // FUTURE TASK: DATABASE INTEGRATION
+  const hostName = req.hostname;
+
+  const subdomain = hostName.split(".")[0];
+  // FUTURE TASK: CUSTOM DOMAIN SUPPORT
+
+  const resolvesTo = `${BasePath}/${subdomain}`;
+  proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    proxyReq.path += "index.html";
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
